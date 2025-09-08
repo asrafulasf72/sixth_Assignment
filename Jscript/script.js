cddb7fde7caebe3d0cd048a9afc85acbdeba35df
asraful <asrafulislamasf@gmail.com>
2025-09-09 02:11:52 +0600

    Add to cart functionality added

--- Jscript/script.js
+++ Jscript/script.js
@@ -1,6 +1,7 @@
 const categoriesContainer=document.getElementById("categories-container")
 const allPlanttreeContainer=document.getElementById("card-container")
-
+const cartContainer=document.getElementById("cart-container")
+let CartArr= [];
 
 const loadaCategoriesContainer= async()=>{
        const url='https://openapi.programming-hero.com/api/categories'
@@ -86,12 +87,12 @@ const loadTreeCategory =(plantsId)=>{
    allPlanttreeContainer.innerHTML=""
    plantsId.forEach(plantId=>{
       allPlanttreeContainer.innerHTML+=`
-             <div class="bg-white rounded-[6px] space-y-3 shadow-sm">
+             <div class="Cart bg-white rounded-[6px] space-y-3 shadow-sm">
                           <div class="">
                              <img class=" w-full h-[186px] rounded-t-[6px]" src="${plantId.image}" alt="">
                           </div>
                           <div class="px-4">
-                              <h2 class="text-2xl font-bold">${plantId.name}</h2>
+                              <div><h2 class="text-2xl font-bold">${plantId.name}</h2></div>
                               <p class="text-[1rem] font-medium">${plantId.description}</p>
                           </div>
                           <div class=" flex justify-between px-4">
@@ -99,7 +100,7 @@ const loadTreeCategory =(plantsId)=>{
                              <p><i class="fa-solid fa-bangladeshi-taka-sign font-light"></i><span class="font-semibold">${plantId.price}</span></p>
                           </div>
                           <div class="px-4">
-                             <button class="bg-green-700 p-2 rounded-3xl w-full text-[1rem] text-white font-medium ">Add to cart</button>
+                             <button class="a bg-green-700 p-2 rounded-3xl w-full text-[1rem] text-white font-medium ">Add to cart</button>
                           </div>
                       </div>
       
@@ -108,4 +109,40 @@ const loadTreeCategory =(plantsId)=>{
    })
 }
 
+// Add Conatiner Your Cart Section
 
+allPlanttreeContainer.addEventListener('click', (e) => {
+    if (e.target.innerText == "Add to cart") {
+        yourCartHandler(e)
+    }
+});
+
+const yourCartHandler=(e)=>{
+    
+       const cartName=e.target.parentNode.parentNode.children[1].children[0].innerText
+        const cartPrice=e.target.parentNode.parentNode.children[2].children[1].innerText
+
+        CartArr.push({
+           name:cartName,
+           price:cartPrice
+        })
+        alert(cartName + " has been added to the cart")
+    displayYourCart(CartArr)
+}
+
+
+const displayYourCart=(CartArr)=>{
+   cartContainer.innerHTML=""
+     CartArr.forEach(cart=>{
+      cartContainer.innerHTML+=`
+             <div class="bg-[#cff0dc] rounded-xl p-2 my-2 flex justify-between items-center">
+                                <div>
+                                      <h1 class="text-[1rem] font-medium">${cart.name}</h1>
+                                 <p><i class="fa-solid fa-bangladeshi-taka-sign font-light"></i>${cart.price}</p>
+                                </div>
+                                <button><i class="fa-solid fa-xmark"></i></button>
+                             </div>
+      
+      `
+     })
+}
